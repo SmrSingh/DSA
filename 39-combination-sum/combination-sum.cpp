5df2df81d99eb79918cbb0c5c74b1020b6697cab
Smriti Singh <166974262+SmrSingh@users.noreply.github.com>
2025-11-18 20:19:18 +0530

    Time: 14 ms (21.25%) | Memory: 17.5 MB (27.29%) - LeetSync

---
+++ 39-combination-sum/combination-sum.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<int>> combinationSum(vector<int>& nums, int target) {
+     vector <vector <vector <int>>> dp(target+1);
+        dp[0]={{}};
+        for(int &i:nums)
+        {
+		// Iterating through all the elements from array nums aka candidates
+            for(int j=i;j<=target;j++)
+            {
+			// Finding all possible ways to achieve sum j from element i
+                for(auto v:dp[j-i])
+                {
+                    v.push_back(i);
+                    dp[j].push_back(v);
+                }
+            }
+        }
+		// Finally, returning our ans \U0001f643
+        return dp[target];   
+    }
+};
